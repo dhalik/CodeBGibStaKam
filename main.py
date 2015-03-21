b6b645831e0091ed7c47600dbe2946aa8b0c1683
Stanley Fung <sfung@v1020-wn-210-231.campus-dynamic.uwaterloo.ca>
2015-03-21 02:08:19 -0400

    Removed double primary key in ORDERSTABLE and added recording of orders in main

--- main.py
+++ main.py
@@ -14,5 +14,22 @@ while (True):
     vols= val["VOL"]
     for tick, nw, dr, vol in zip(tickers, nws, drs, vols):
         SQLService.insertStock(period, tick, nw, dr, vol)
+
+    val = Action(ActionType.ORDERS).run()
+    transType = "BID"
+    tickers = val["TICKER"]
+    price= val["BID_PRICE"]
+    quantity= val["BID_QUANTITY"]
+    for transType,tickers, price, quantity in zip(transType,tickers, price, quantity):
+        SQLService.insertOrder(transType, period, tickers, price, quantity) 
+ 	
+    transType = "ASK"
+    tickers = val["TICKER"]
+    price= val["ASK_PRICE"]
+    quantity= val["ASK_QUANTITY"]
+    for transType,tickers, price, quantity in zip(transType,tickers, price, quantity):
+        SQLService.insertOrder(transType, period, tickers, price, quantity) 
+    
     time.sleep(1)
     period = period + 1
+
