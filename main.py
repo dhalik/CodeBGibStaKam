9257fea450b697ba41ad19f73731d15737279c46
Stanley Fung <sfung@v1020-wn-210-231.campus-dynamic.uwaterloo.ca>
2015-03-21 02:21:54 -0400

    Moduralized main a bit

--- main.py
+++ main.py
@@ -4,32 +4,57 @@ import time
 import SubscriberService
 from ActionType import *
 
-SQLService.connectToDB()
 period = 0;
-while (True):
-    val = Action(ActionType.SECURITIES).run()
-    tickers = val["TICKER"]
-    nws= val["NET_WORTH"]
-    drs= val["DIV_RATIO"]
-    vols= val["VOL"]
-    for tick, nw, dr, vol in zip(tickers, nws, drs, vols):
-        SQLService.insertStock(period, tick, nw, dr, vol)
+AAPL_TICKER = 'AAPL'
+ATVI_TICKER = 'ATVI'
+EA_TICKER = 'EA'
+FB_TICKER = 'FB'
+GOOG_TICKER = 'GOOG'
+MSFT_TICKER = 'MSFT'
+SNY_TICKER = 'SNY'
+TSLA_TICKER = 'TSLA'
+TWTR_TICKER = 'TWTR'
+XOM_TICKER = 'XOM'
 
-    val = Action(ActionType.ORDERS).run()
-    transType = "BID"
-    tickers = val["TICKER"]
-    price= val["BID_PRICE"]
-    quantity= val["BID_QUANTITY"]
+def runOrder(ticker):
+ 	val = Action(ActionType.ORDERS, ticker).run()
+	transType = "BID"
+	tickers = val["TICKER"]
+	price= val["BID_PRICE"]
+	quantity= val["BID_QUANTITY"]
     for transType,tickers, price, quantity in zip(transType,tickers, price, quantity):
         SQLService.insertOrder(transType, period, tickers, price, quantity) 
  	
-    transType = "ASK"
+ 	transType = "ASK"
     tickers = val["TICKER"]
     price= val["ASK_PRICE"]
     quantity= val["ASK_QUANTITY"]
     for transType,tickers, price, quantity in zip(transType,tickers, price, quantity):
         SQLService.insertOrder(transType, period, tickers, price, quantity) 
-    
-    time.sleep(1)
-    period = period + 1
+	
+def runSecurities():
+	val = Action(ActionType.SECURITIES).run()
+	    tickers = val["TICKER"]
+	    nws= val["NET_WORTH"]
+	    drs= val["DIV_RATIO"]
+	    vols= val["VOL"]
+	    for tick, nw, dr, vol in zip(tickers, nws, drs, vols):
+	        SQLService.insertStock(period, tick, nw, dr, vol)
+
+if (__name__ == "__main__"):
+	SQLService.connectToDB()
+	while (True):
+		runSecurities()
+		runOrder(GOOG_TICKER)
+		runOrder(AAPL_TICKER)
+		runOrder(ATVI_TICKER)
+		runOrder(EA_TICKER)
+		runOrder(FB_TICKER)
+		runOrder(MSFT_TICKER)
+		runOrder(SNY_TICKER)
+		runOrder(TSLA_TICKER)
+		runOrder(TWTR_TICKER)
+		runOrder(XOM_TICKER)
+	    time.sleep(1)
+	    period = period + 1
 
