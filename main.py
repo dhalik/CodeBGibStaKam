0faf61954af3e85c534009fa3acf044a46cb6987
Kam Dhaliwal <kamaljot.dhaliwal@gmail.com>
2015-03-21 04:09:09 -0400

    merging

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import SQLService
 import time
 import SubscriberService
 from ActionType import *
+import stock
 
 period = 0;
 AAPL_TICKER = 'AAPL'
@@ -15,8 +16,9 @@ SNY_TICKER = 'SNY'
 TSLA_TICKER = 'TSLA'
 TWTR_TICKER = 'TWTR'
 XOM_TICKER = 'XOM'
+stockList = {}
 
-def runOrder(ticker):
+def getBidAsk(ticker):
 	val = Action(ActionType.ORDERS, ticker).run()
 	transType = "BID"
 	tickers = val["TICKER"]
@@ -25,6 +27,8 @@ def runOrder(ticker):
 	for transType,tickers, price, quantity in zip(transType,tickers, price, quantity):
 		SQLService.insertOrder(transType, period, tickers, price, quantity)
 
+	bestBid = max(price)
+
 	transType = "ASK"
 	tickers = val["TICKER"]
 	price= val["ASK_PRICE"]
@@ -32,29 +36,35 @@ def runOrder(ticker):
 	for transType,tickers, price, quantity in zip(transType,tickers, price, quantity):
 		SQLService.insertOrder(transType, period, tickers, price, quantity)
 
+	bestAsk = max(price)
+
+	return (bestBid, bestAsk)
+
 def runSecurities():
+	global stockList;
+
 	val = Action(ActionType.SECURITIES).run()
 	tickers = val["TICKER"]
 	nws= val["NET_WORTH"]
 	drs= val["DIV_RATIO"]
 	vols= val["VOL"]
+
 	for tick, nw, dr, vol in zip(tickers, nws, drs, vols):
 		SQLService.insertStock(period, tick, nw, dr, vol)
+		if (tick in stockList):
+			spread = getBidAsk()
+			midMarket = spread[0] + (spread[0] - spread[1])/2
+			print "Midmarket is " + str(midMarket)
+			stockList.addTick('',midMarket,dr,'',vol, '');
+
+def createStocks():
+	global stockList;
+	stockList[GOOG_TICKER] = Stock(GOOG_TICKER);
+	stockList[EA_TICKER] = Stock(EA_TICKER);
 
 if (__name__ == "__main__"):
 	SQLService.connectToDB()
 	while (True):
-		runSecurities()
-		runOrder(GOOG_TICKER)
-		runOrder(AAPL_TICKER)
-		runOrder(ATVI_TICKER)
-		runOrder(EA_TICKER)
-		runOrder(FB_TICKER)
-		runOrder(MSFT_TICKER)
-		runOrder(SNY_TICKER)
-		runOrder(TSLA_TICKER)
-		runOrder(TWTR_TICKER)
-		runOrder(XOM_TICKER)
 		time.sleep(1)
 		period = period + 1
 
